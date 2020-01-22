b4b286987fc9efba2607d1cd20b1fb421e5d3348
Victor <Victortornbom@gmail.com>
2020-01-22 14:53:50 +0100

    Added fade
    victor, anton, andre

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="movie-bg-image valign-wrapper center-align">
         <i class="material-icons large icon-white valign-wrapper center-align btn-flat">play_circle_filled</i>
-        <img class="img-bg" src="http://trumpwallpapers.com/wp-content/uploads/Joker-2019-Wallpaper-01-2560-x-1440.jpg">
+        <img class="img-bg" src="https://trumpwallpapers.com/wp-content/uploads/Joker-2019-Wallpaper-01-2560-x-1440.jpg">
     </div>
 </template>
 
@@ -15,6 +15,8 @@ export default {
 .img-bg{
     width: 100%;
     max-height: 100vh;
+    -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+      mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0.2));
 }
 
 .movie-bg-image{

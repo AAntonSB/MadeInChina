dd307b3ac8500c6e874bf9536f527b2255607b18
Victor <Victortornbom@gmail.com>
2020-01-22 13:46:42 +0100

    Added image and playbutton
    victor, anton

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,17 +1,34 @@
 <template>
-    <div>
-        
+    <div class="movie-bg-image valign-wrapper center-align">
+        <i class="material-icons large icon-white valign-wrapper center-align btn-flat">play_circle_filled</i>
+        <img class="img-bg" src="https://images3.alphacoders.com/975/975999.png">
     </div>
 </template>
 
 <script>
 export default {
+
 }
 </script>
 
 <style>
 .img-bg{
-    width:100vw;
-    height: 70vh;
+    max-width: 100%;
+    max-height: 100vh;
 }
+
+.movie-bg-image{
+    padding: 0;
+    margin: 0;
+}
+
+.material-icons{
+    position: relative;
+    left: 50%;
+    transform: translate(-50%);
+}
+i.icon-white{
+    color:white;   
+}
+
 </style>

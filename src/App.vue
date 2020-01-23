e60980c839575bf7332c10da67aaffd174b6cbe0
AAntonSB <anton.s.bergqvist@gmail.com>
2020-01-23 09:12:04 +0100

    Shadow Overlay, close overlay options

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 
 <template>
   <div id="app">
-    <router-link to="/movies">ajsdasdh</router-link>
     <router-view></router-view>
   </div>
 </template>
@@ -17,5 +16,57 @@ body{
   margin: 0;
   display:flex;
   flex-direction: column;
+}
+.overlay{
+  height: 100%;
+  width: 100%;
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0px;
+  top: 0px;
+  background-color: rgb(0,0,0); /* Black fallback color */
+  background-color: rgba(0,0,0, 0.75); /* Black w/opacity */
+  overflow-x: hidden; /* Disable horizontal scroll */
+  transition: 0.5s; /* 0.5 second transition effect to slide in or slide down the overlay (height or width, depending on reveal) */
+}
+.overlay-content {
+  position: relative;
+  top: 25%; /* 25% from the top */
+  width: 100%; /* 100% width */
+  text-align: center; /* Centered text/links */
+  margin-top: 30px; /* 30px top margin to avoid conflict with the close button on smaller screens */
+}
+
+/* The navigation links inside the overlay */
+.overlay a {
+  padding: 8px;
+  text-decoration: none;
+  font-size: 36px;
+  color: #818181;
+  display: block; /* Display block instead of inline */
+  transition: 0.3s; /* Transition effects on hover (color) */
+}
+
+/* When you mouse over the navigation links, change their color */
+.overlay a:hover, .overlay a:focus {
+  color: #f1f1f1;
+}
+
+/* Position the close button (top right corner) */
+.overlay .closebtn {
+  position: absolute;
+  top: 20px;
+  right: 45px;
+  font-size: 60px;
+}
+
+/* When the height of the screen is less than 450 pixels, change the font-size of the links and position the close button again, so they don't overlap */
+@media screen and (max-height: 450px) {
+  .overlay a {font-size: 20px}
+  .overlay .closebtn {
+    font-size: 40px;
+    top: 15px;
+    right: 35px;
+  }
 }
 </style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,63 +1,92 @@
 <template>
-    <div class="movie-bg-image valign-wrapper center-align">
-        <i @click="showTrailer()" class="material-icons large icon-white valign-wrapper center-align btn-flat">play_circle_filled</i>
-        <img @click="hideTrailer()" class="img-bg" src="https://trumpwallpapers.com/wp-content/uploads/Joker-2019-Wallpaper-01-2560-x-1440.jpg">
-        <div class="video-player" v-if="this.trailerVisible === true">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zAGVQLHvwOY/D6Ac5JpCHmI?&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+  <div class="movie-bg-image valign-wrapper">
+    <img
+      class="img-bg"
+      src="https://trumpwallpapers.com/wp-content/uploads/Joker-2019-Wallpaper-01-2560-x-1440.jpg"
+    />
+    <i @click="toggleTrailer()" class="material-icons large icon-white valign-wrapper center-align btn-flat">play_circle_filled</i>
+
+    <div @click="toggleTrailer()" class="overlay" v-if="this.trailerVisible === true">
+        <div class="video-player overlay-content">
+            <div class="video-frame">
+                <a class="button btn-flat btn-large waves-effect transparent"><i class="material-icons">close</i></a>
+                <iframe width="920" height="517.5" src="https://www.youtube.com/embed/zAGVQLHvwOY/D6Ac5JpCHmI?&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+            </div>
         </div>
     </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-data(){
-    return{
-        trailerVisible: false,
-    }
-},
-methods:{
-    showTrailer(){
-        if(this.trailerVisible === false){
-            return this.trailerVisible = true;
-        }
-        return this.trailerVisible = false
+  data() {
+    return {
+      trailerVisible: false
+    };
+  },
+  methods: {
+    toggleTrailer() {
+      this.trailerVisible = !this.trailerVisible;
     },
-    hideTrailer(){
-        if(this.trailerVisible === true){
-            return this.trailerVisible = false;
+    handleKeyPress(e){
+        if(e.keyCode === 27 && this.trailerVisible){
+            this.toggleTrailer();
         }
     }
-}
+  },
+    created() {
+        window.addEventListener('keydown', this.handleKeyPress)
+    },
+    destroyed(){
+        window.removeEventListener('keydown', this.handleKeyPress)
+    }
+
 }
 </script>
 
 <style>
-.img-bg{
-    width: 100%;
-    max-height: 100vh;
-    -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
-      mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0.2));
+.img-bg {
+  width: 100%;
+  max-height: 100vh;
+  -webkit-mask-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(rgba(0, 0, 0, 1)),
+    to(rgba(0, 0, 0, 0)));
+    mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.2));
 }
 
-.movie-bg-image{
-    padding: 0;
+.movie-bg-image {
+  padding: 0;
     margin: 0;
 }
 
-.material-icons{
-    position: absolute;
+.material-icons {
+  position: absolute;
     left: 50%;
     transform: translate(-50%);
     z-index: 1;
 }
-i.icon-white{
-    color:white;   
+i.icon-white {
+  color: white;
 }
-.video-player{
+.video-player {
+  margin: 0;
     position: absolute;
-    left: 50%;
-    transform: translate(-50%);
-    z-index: 2;
+    top: 50%;
+    transform: translateY(-60%);
 }
-
+.button {
+  left: 52%;
+}
+.video-frame {
+  width: 920px;
+  height: 517.5px;
+}
+.hej {
+  position: absolute;
+}
 </style>

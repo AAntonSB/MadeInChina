a378c9668fa31e8501526847370c6d9d63aad75f
unknown <joakim.grohn@gmail.com>
2020-01-27 15:32:50 +0100

    filled out placehold screenings

--- src/store/placeholderscreenings.js
+++ src/store/placeholderscreenings.js
@@ -1,15 +1,739 @@
 
 export const placeholderscreenings = [
-    {
+
+        /*
+        
+        */
+        //Monday
+        //2rd of march 2020. 2115
+    {   
         screeningID: 1,
         auditoriumName: "Stora",
-        //auditoriumSize: 80,
-
-        //film ID
-
-        date: {day: 1, month: 1, year: 2020},
-        time: {hour:20, minute: 10},
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 2, 19, 0),
         film: "Joker",
-    }
+    },
+    {   
+        screeningID: 2,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 2, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 3,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 2, 21, 15),
+        film: "Joker",
+    },
+        //Tuesday
+        //3rd of march 2020. 1900
+    {   
+        screeningID: 4,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 3, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 5,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 3, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 6,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 3, 21, 15),
+        film: "Joker",
+    },
+        //Wednesday
+        //4rd of march 2020. 1900
+    {   
+        screeningID: 7,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 4, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 8,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 4, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 9,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 4, 21, 15),
+        film: "Joker",
+    },
+        //Thursday
+        //5rd of march 2020. 1900
+    {   
+        screeningID: 10,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        
+        date: new Date(2020, 2, 5, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 11,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 5, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 12,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 5, 21, 15),
+        film: "Joker",
+    },
+        //Friday
+        //6rd of march 2020. 1900
+    {   
+        screeningID: 13,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 6, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 14,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 6, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 15,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 6, 21, 15),
+        film: "Joker",
+    },
+        //Saturday
+        //7th of march 2020. 1900
+    {   
+        screeningID: 16,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 7, 19, 0),
+        film: "Joker",
+        },
+        {   
+        screeningID: 17,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 7, 19, 0),
+        film: "Frozen 2",
+        },
+        {   
+        screeningID: 18,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 7, 21, 15),
+        film: "Joker",
+        },
+        //Sunday
+        //8th of march 2020. 1900
+        {   
+        screeningID: 19,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 8, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 20,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 8, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 21,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 8, 21, 15),
+        film: "Joker",
+    },
+        //Monday
+        //9th of march 2020. 2115
+    {   
+        screeningID: 22,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 9, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 23,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 9, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 24,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 9, 21, 15),
+        film: "Joker",
+    },
+        //Tuesday
+        //10th of march 2020. 1900
+    {   
+        screeningID: 25,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 10, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 26,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 10, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 27,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 10, 21, 15),
+        film: "Joker",
+    },
+        //Wednesday
+        //11th of march 2020. 1900
+    {   
+        screeningID: 28,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 11, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 29,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 11, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 30,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 11, 21, 15),
+        film: "Joker",
+    },
+        //Thursday
+        //12th of march 2020. 1900
+    {   
+        screeningID: 31,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        
+        date: new Date(2020, 2, 12, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 32,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 12, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 33,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 12, 21, 15),
+        film: "Joker",
+    },
+        //Friday
+        //13th of march 2020. 1900
+    {   
+        screeningID: 34,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 13, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 35,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 13, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 36,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 13, 21, 15),
+        film: "Joker",
+    },
+        //Saturday
+        //14th of march 2020. 1900
+    {   
+        screeningID: 37,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 14, 19, 0),
+        film: "Joker",
+        },
+        {   
+        screeningID: 38,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 14, 19, 0),
+        film: "Frozen 2",
+        },
+        {   
+        screeningID: 39,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 14, 21, 15),
+        film: "Joker",
+        },
+        //Sunday
+        //15th of march 2020. 1900
+        {   
+        screeningID: 40,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 15, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 41,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 15, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 42,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 15, 21, 15),
+        film: "Joker",
+    },
+        //Monday
+        //16th of march 2020. 1900
+    {   
+        screeningID: 43,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 16, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 44,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 16, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 45,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 16, 21, 15),
+        film: "Joker",
+    },
+        //Tuesday
+        //17thd of march 2020. 1900
+    {   
+        screeningID: 46,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 17, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 47,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 17, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 48,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 17, 21, 15),
+        film: "Joker",
+    },
+        //Wednesday
+        //18th of march 2020. 1900
+    {   
+        screeningID: 49,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 18, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 50,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 18, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 51,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 18, 21, 15),
+        film: "Joker",
+    },
+        //Thursday
+        //19th of march 2020. 1900
+    {   
+        screeningID: 52,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 19, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 53,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 19, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 54,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 19, 21, 15),
+        film: "Joker",
+    },
+        //Friday
+        //20th of march 2020. 1900
+    {   
+        screeningID: 55,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 20, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 56,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 20, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 57,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 20, 21, 15),
+        film: "Joker",
+    },
+        //Saturday
+        //21th of march 2020. 1900
+    {   
+        screeningID: 58,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 21, 19, 0),
+        film: "Joker",
+        },
+        {   
+        screeningID: 59,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 21, 19, 0),
+        film: "Frozen 2",
+        },
+        {   
+        screeningID: 60,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 21, 21, 15),
+        film: "Joker",
+        },
+        //Sunday
+        //22th of march 2020. 1900
+        {   
+        screeningID: 61,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 22, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 62,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 22, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 63,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 22, 21, 15),
+        film: "Joker",
+    },
+        //Monday
+        //23th of march 2020. 2115
+    {   
+        screeningID: 64,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 23, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 65,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 23, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 66,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 23, 21, 15),
+        film: "Joker",
+    },
+        //Tuesday
+        //24th of march 2020. 1900
+    {   
+        screeningID: 67,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 24, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 68,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 24, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 69,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 24, 21, 15),
+        film: "Joker",
+    },
+        //Wednesday
+        //25th of march 2020. 1900
+    {   
+        screeningID: 70,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 25, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 71,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 25, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 72,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 25, 21, 15),
+        film: "Joker",
+    },
+        //Thursday
+        //26th of march 2020. 1900
+    {   
+        screeningID: 73,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 26, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 74,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 26, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 75,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 26, 21, 15),
+        film: "Joker",
+    },
+        //Friday
+        //27th of march 2020. 1900
+    {   
+        screeningID: 76,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 27, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 77,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 27, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 78,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 27, 21, 15),
+        film: "Joker",
+    },
+        //Saturday
+        //28th of march 2020. 1900
+    {   
+        screeningID: 79,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 28, 19, 0),
+        film: "Joker",
+        },
+        {   
+        screeningID: 80,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 28, 19, 0),
+        film: "Frozen 2",
+        },
+        {   
+        screeningID: 81,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 28, 21, 15),
+        film: "Joker",
+        },
+        //Sunday
+        //29th of march 2020. 1900
+        {   
+        screeningID: 82,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 29, 19, 0),
+        film: "Joker",
+    },
+    {   
+        screeningID: 83,
+        auditoriumName: "Lilla",
+        movieID: 1002,
+        length: 103,
+        date: new Date(2020, 2, 29, 19, 0),
+        film: "Frozen 2",
+    },
+    {   
+        screeningID: 84,
+        auditoriumName: "Stora",
+        movieID: 1001,
+        length: 122,
+        date: new Date(2020, 2, 29, 21, 15),
+        film: "Joker",
+    },
+
 
 ]

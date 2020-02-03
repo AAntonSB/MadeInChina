b68d835e02361d03add4cc8a50c17f9984c0f54c
AAntonSB <anton.s.bergqvist@gmail.com>
2020-02-03 14:42:04 +0100

    Register account functionality

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,25 +1,52 @@
 <template>
   <div class="bg-modal">
     <div class="modal-content">
-    <form class="register-form col s12">
-      <p style="font-size: 30px">LOGO</p>
-      <h5>Registrera ett placeholder-konto</h5>
-      <div class="email">
-        <input class="col s12 input" type="email" placeholder="E-Mail" />
+      <div class="row valign-wrapper">
+        <form class="register-form col s12">
+          <p style="font-size: 30px">LOGO</p>
+          <h5>Registrera ett placeholder-konto</h5>
+          <div class="email">
+            <input class="col s12 input" type="email" placeholder="E-Mail" v-model="email" />
+          </div>
+          <input class="col s12" type="password" placeholder="Lösenord" v-model="password" />
+          <input
+            class="col s12"
+            type="password"
+            placeholder="Bekräfta Lösenord"
+            v-model="confirmedPassword"
+          />
+          <div class="signup">
+            <a class="btn-large create-account-button" @click="registerAccount()">Skapa Konto</a>
+            <strong>
+              <router-link to="/LoginModal" class="left">Logga in i stället</router-link>
+            </strong>
+          </div>
+        </form>
       </div>
-      <input class="col s12" type="password" placeholder="Lösenord" />
-      <input class="col s12" type="password" placeholder="Bekräfta Lösenord" />
-      <div class="signup">
-        <a class="btn-large create-account-button">Skapa Konto</a>
-        <strong>
-          <router-link to="/LoginModal" class="left">Logga in i stället</router-link>
-        </strong>
-      </div>
-    </form>
     </div>
   </div>
 </template>
 
+<script>
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirmedPassword: ""
+    };
+  },
+  methods: {
+        async registerAccount() {
+            const user = firebase.auth().createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
+              console.log( user + error.code)
+            })
+    },
+  }
+};
+</script>
 
 <style scoped>
 .bg-modal {

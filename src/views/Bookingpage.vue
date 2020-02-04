e33073286371702bde66f42a3cb84d2cea0bb272
André <andreingvarsson@hotmail.com>
2020-02-04 10:56:39 +0100

    Added bookingpage
    Anton, victor, Danilla.

---
+++ src/views/Bookingpage.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+<div>
+<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
+</div>
+</template>
+    

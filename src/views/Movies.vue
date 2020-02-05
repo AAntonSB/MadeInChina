47439cea871814e0712ed6632f14365b24c23a98
Yusra <yossan@hotmail.se>
2020-02-05 10:35:12 +0100

    fixed merge

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< Updated upstream
         <div class="flexdirectioncolumn">
             <!-- Big picture with search -->
             <!-- Hooper -->
@@ -107,89 +106,6 @@
 }
 </style>
 
-=======
-  <div class="flexdirectioncolumn">
-    <!-- Big picture with search -->
-    <div class="bigimgmovies">
-      <div class="bigimginnerbckg">
-        <a class="dropdown-trigger btn" data-target="dropdown1">Sök via dag</a>
-   
-
-        <!-- Dropdown Structure -->
-        <div class="dropdown-content">
-         
-          <ul id="dropdown1" class="dropdown-content">
-            <li>
-              <a>måndag</a>
-            </li>
-            <li>
-              <a>tisdag</a>
-            </li>
-            
-            <li>
-              <a>onsdag</a>
-            </li>
-            <li>
-              <a>torsdag</a>
-            </li>
-            <li>
-              <a>fredag</a>
-            </li>
-            <li>
-              <a>lördag</a>
-            </li>
-            <li>
-              <a>söndag</a>
-              </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <!-- Hooper -->
-    <HooperSlider msg="Text" />
-
-    <!-- Carousel -->
-    <hooper :progress="true" :autoPlay="true" :playSpeed="3000" style="width: 100%; height:100%;">
-      <slide v-for="movie in movies" :key="movie.id">
-        <div>
-          <img :src="movie.imageOfCarousel" />
-        </div>
-      </slide>
-      <hooper-navigation slot="hooper-addons"></hooper-navigation>
-      <hooper-pagination slot="hooper-addons"></hooper-pagination>
-    </hooper>
-  </div>
-</template>
-
-<style>
-.bigimgmovies {
-  background-image: url("https://catalog.cinema-api.com/images/ncg-images/85125f023d564412b78598aecc388c4e.jpg?width=1920&version=5FFD6A056267F02198B2ABDD2DCAD9F1");
-  background-color: #000;
-  height: 550px;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  margin-bottom: 15px;
-}
-.bigimginnerbckg {
-  background: linear-gradient(
-    -180deg,
-    transparent 50%,
-    rgba(0, 0, 0, 0.68) 85%,
-    #000
-  );
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  color: #fff;
-  padding-top: 20%;
-}
-</style>
->>>>>>> Stashed changes
 <script>
 import {
   Hooper,

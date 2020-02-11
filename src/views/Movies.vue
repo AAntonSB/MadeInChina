de2131d217f9d86fe3598a3757e824462b5655c4
Yusra <yossan@hotmail.se>
2020-02-11 11:09:57 +0100

    Changed csss in booking button.

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -21,7 +21,7 @@
                         </span>
                         <div class="bookingButtonBox">
                             <router-link :to="{path: '/bookingpage', query: { showtimeId: 1 }}">
-                              <button class="waves-effect waves-light book-button">Boka</button>
+                              <button class="waves-effect waves-light book-btn">Boka</button>
                             </router-link>
                         </div>
                     </div>
@@ -205,7 +205,7 @@
   vertical-align: middle;
   visibility: hidden;
 }
-.book-button{    
+.book-btn{    
   width: 200px;
   padding: 12px;
   background-color: #C62828;

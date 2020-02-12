8a4d5bfbe8f78cc485c7f280ca8a95fc1dc30ba2
Aniko Magyar <mofolade@gmail.com>
2020-02-12 14:49:47 +0100

    display of sold places

--- src/views/Bookingpage.vue
+++ src/views/Bookingpage.vue
@@ -3,7 +3,7 @@
       <div class="flexdirectioncolumn  bookingbox">
         <div id="ticketSelectorSection">
           <div id="bookingPanel">
-            <h2>Antal biljetter</h2>{{getAuditoriumId()}}
+            <h2>Antal biljetter</h2><label>{{this.auditoriumSize-this.bookedSeatsCount}} kvar av {{this.auditoriumSize}}</label>
             <!-- Ordinary -->
             <div class="flexdirectionrow ticketSection">
               <div class="ticketTypeTextBox">
@@ -13,7 +13,7 @@
               <div class="ticketCounter">
                 <i id="ordinaryMinusIcon" class="material-icons booking-icon" v-on:click="ordinaryTicketCount--, allTypesCount--" v-on:click.capture="checkCounter('ordinary')">remove_circle_outline</i>
                 <span class="countNumber" id="ordinarycount">{{ordinaryTicketCount}}</span>
-                <i class="material-icons booking-icon" v-on:click="ordinaryTicketCount++, allTypesCount++" v-on:click.capture="checkCounter('ordinary')">add_circle_outline</i>
+                <i id="ordinaryPlusIcon" class="material-icons booking-icon" v-on:click="ordinaryTicketCount++, allTypesCount++" v-on:click.capture="checkCounter('ordinary')">add_circle_outline</i>
               </div>
             </div>
             <!-- Retired -->
@@ -25,7 +25,7 @@
               <div class="ticketCounter">
                 <i id="retiredMinusIcon" class="material-icons booking-icon" v-on:click="retiredTicketCount--, allTypesCount--" v-on:click.capture="checkCounter('retired')">remove_circle_outline</i>
                 <span class="countNumber" id="countNumber retiredcount">{{retiredTicketCount}}</span>
-                <i class="material-icons booking-icon" v-on:click="retiredTicketCount++ , allTypesCount++" v-on:click.capture="checkCounter('retired')">add_circle_outline</i>
+                <i id="retiredPlusIcon" class="material-icons booking-icon" v-on:click="retiredTicketCount++ , allTypesCount++" v-on:click.capture="checkCounter('retired')">add_circle_outline</i>
               </div>
             </div>
             <!-- Child -->
@@ -37,7 +37,7 @@
               <div class="ticketCounter">
                 <i id="childMinusIcon" class="material-icons booking-icon" v-on:click="childTicketCount--, allTypesCount--" v-on:click.capture="checkCounter('child')">remove_circle_outline</i>
                 <span class="countNumber" id="countNumber childcount">{{childTicketCount}}</span>
-                <i class="material-icons booking-icon" v-on:click="childTicketCount++, allTypesCount++" v-on:click.capture="checkCounter('child')">add_circle_outline</i>
+                <i id="childPlusIcon" class="material-icons booking-icon" v-on:click="childTicketCount++, allTypesCount++" v-on:click.capture="checkCounter('child')">add_circle_outline</i>
               </div>
             </div>          
           </div>
@@ -47,10 +47,10 @@
         </div>
         <div class="buttonsBox">
           <button id="bookingBtn" class="btn-small red" v-on:click="showScene()">Välj platser</button>
-          <button id="changeBtn" class="btn-small grön" v-on:click="showPanel()">Ändra</button>          
+          <button id="changeBtn" class="btn-small grön" v-on:click="showPanel()">Ändra</button>         
           <button id="saveBtn" class="btn-small red" v-on:click="saveBooking()">Spara</button>
         </div>        
-        <div id="scenePanel" class="avoid-clicks">          
+        <div id="scenePanel" class="avoid-clicks">       
           <div class="scene"></div>
           <div v-for="rowindex in seatsRow" :key="rowindex">
             <div class="seatsrow">
@@ -66,11 +66,14 @@
 </template>
     
 <script>
+
 export default {
   data() {
     return {      
       showtimeId: this.$route.query.showtimeId,
+      auditorium:[],
       auditoriumId: 0,
+      auditoriumSize: 0,
       ordinaryTicketPris: 85,
       retiredTicketPris: 75,
       childTicketPris: 65,
@@ -80,6 +83,8 @@ export default {
       allTypesCount: 0,
       seatsRow: 0,
       seats: [],
+      bookings:[],
+      bookedSeatsCount: 0,
       choosenSeatCount: 0
     };
   },
@@ -109,6 +114,11 @@ export default {
       }else{
         document.getElementById('bookingBtn').style.display= "none";
         document.getElementById('scenePanel').classList.add('avoid-clicks');
+      }
+      if((this.auditoriumSize-this.bookedSeatsCount) == this.allTypesCount){
+        document.getElementById('ordinaryPlusIcon').style.visibility= "hidden";
+        document.getElementById('retiredPlusIcon').style.visibility= "hidden";
+        document.getElementById('childPlusIcon').style.visibility= "hidden";
       }
     },
     showScene: function() {
@@ -156,10 +166,22 @@ export default {
         }
       }
     },
-    getAuditoriumId(){ 
-        let auditorium = this.$store.getters.getSingleShowtimeById(this.$route.query.showtimeId);
-        this.auditoriumId = auditorium[0].auditoriumId;
-        this.seatsRow = this.$store.getters.getAuditorium(this.auditoriumId)[0].seatsPerRow.length
+    setAuditoriumId(){ 
+        let singleShowtimeById = this.$store.getters.getSingleShowtimeById(this.$route.query.showtimeId);
+        this.auditoriumId = singleShowtimeById[0].auditoriumId;
+        this.seatsRow = this.$store.getters.getAuditorium(this.auditoriumId)[0].seatsPerRow.length;
+        this.auditoriumSize = this.$store.getters.getAuditorium(this.auditoriumId)[0].seats;
+    },
+    getBookedSeats(){
+      this.bookings=this.$store.getters.getAllBookingsByShowtimeId(this.$route.query.showtimeId);
+      this.bookedSeatsCount=this.bookings.length;
+    },
+    setBookedSeats(){
+        for(let b = 0; b < this.bookings.length; b++){
+          let bookedSeatId = this.bookings[b].row+'_'+this.bookings[b].col;
+          document.getElementById(bookedSeatId).classList.add("soldSeat");
+        }
+        return null;
     },
     saveBooking(){
       alert('coming soon')
@@ -172,23 +194,25 @@ export default {
                   +(this.retiredTicketCount*this.retiredTicketPris)
                   +(this.childTicketCount*this.childTicketPris);
           },
-        set: function() {
-            
+        set: function() {            
         }
-      },
-      getAuditorium() {
-        if (this.auditoriumId > 0){
-          return this.$store.getters.getAuditorium(this.auditoriumId)
-        }
-        else{
-          return null;
-        }
-      },
+    },
+    getAuditorium() {
+      return this.$store.getters.getAuditorium(this.auditoriumId)
+    },
+  },
+  mounted() {
+    this.setAuditoriumId();
+    this.getBookedSeats();
+  },
+  updated(){
+    this.setBookedSeats();
   },
   created() {
-    console.log(this.$route.query.showtimeId);
+    console.log('bookingpage:'+this.$route.query.showtimeId);
     this.$store.dispatch("pullShowtimes");
     this.$store.dispatch("pullBookings");
+    this.$store.dispatch("getBookings");
   },
 };
 </script>
@@ -226,7 +250,7 @@ export default {
   justify-content: space-around;
 }
 .bookingbox h2{
-  font-size: 2.56rem;
+  font-size: 2.36rem;
   margin: 15px 15px;
 }
 .ticketSection{
@@ -325,9 +349,10 @@ export default {
   margin: 15px 0px 0px 0px;
 }
 .reservedSeat{
-  background-color: #ff4d4d;
+  background-color: #ff8383;
 }
 .soldSeat{
   background-color: #e93030;
+  pointer-events: none;
 }
 </style>

eb819e7244c591dc48b9f4ec8bab0ad909b69ca9
André <andreingvarsson@hotmail.com>
2020-02-14 12:52:24 +0100

    adjusting placement of text, and img.
    Anton, Victor

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,12 +1,14 @@
 <template>
-<div class="">
+<div class="row">
        <!-- <section v-for="movie in movies" v-bind:key="movie.title"> -->
            
  <section v-for="show in showtimes" v-bind:key="show">
     
-    <div class="main-body">
-        HELLO{{show.movieId}}
-        <svg class="defs-only">
+    <div class="main-body row">
+       <div class="relative">
+        
+           
+        <!-- <svg class="defs-only">
             <filter id="monochrome" 
             color-interpolation-filters="sRGB"
             x="0" y="0" height="100%" width="100%">
@@ -16,8 +18,16 @@
             0.65 0 0 0 0
             0    0 0 1 0" />
             </filter>
-        </svg>
-        <img class="ticket" src="https://www.pngkey.com/png/full/19-196604_large-raffle-ticket-printable-raffle-tickets.png">
+        </svg> -->
+       
+            <div class="row">
+            <h6 class="col s12">Mån 3/4</h6>
+            <h6 class="col s12">>Tid: 18:00</h6>
+            <h6 class="col s12">Stolsnummer: rad 4, stol 5</h6>
+            <h6>>Bokningsnummer: #213242545</h6>
+            <h6>>Pris: 85:-</h6>
+            </div>
+        </div> 
     </div>
     </section>
     </div>
@@ -48,7 +58,37 @@ created(){
 
 },
 methods:{
+    setDateWithIndex: function(x){
+        // Getting required values
+        let today = new Date()
+        let year = today.getFullYear()
+        let month = today.getMonth()
+        let day = today.getDate()
 
+        // Creating a new Date (with the delta)
+        let finalDate = new Date(year, month, day + x-1)
+
+        day = ''+finalDate.getDate();
+        //let monthIndex = finalDate.getMonth();
+        month = ''+(finalDate.getMonth()+1)
+        year = finalDate.getFullYear();
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        let days = [
+        'söndag',
+        'måndag',
+        'tisdag',
+        'onsdag',
+        'torsdag',
+        'fredag',
+        'lördag'
+        ]
+
+        let dayName = days[finalDate.getDay()]
+
+        return day+'/'+month +' '+ ' - '+dayName;
+        }
 }   
 
 }
@@ -56,17 +96,42 @@ methods:{
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+}
 .ticket{
     -webkit-filter: url(#monochrome);
   filter:  url(#monochrome);
+  justify-content: center;
+    align-items: center;
+}
 
+.test{
+    position: absolute;
+}
+
+.relative{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 446px;
+    height: 223px;
+    background-image: url("https://www.pngkey.com/png/full/19-196604_large-raffle-ticket-printable-raffle-tickets.png");
+    fill: red;
+}
+
+
+.main-body{
+    display:flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .defs-only{
-    position: relative;
-    height: 0; width: 0;
-    overflow: none;
-    left: -100%;
+ 
 }
+
 
 </style>

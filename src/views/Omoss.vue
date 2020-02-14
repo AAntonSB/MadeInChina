3f1bfd864a9ead76fd65442bc461b6df7aaa37c8
Yusra <yossan@hotmail.se>
2020-02-14 11:35:07 +0100

    Fixed footer page "omoss" with text and CSS.

--- src/views/Omoss.vue
+++ src/views/Omoss.vue
@@ -1,68 +1,93 @@
 <template>
     <div class="flexdirectioncolumn">
-        <div id="kontaktPanel">
-           <h6><b>Kontakta oss</b></h6> 
-            Smålandsgatan 53<br>
-            710 00 Småland<br>
+        <div id="kontaktPanel" class="flexdirectioncolumn">
+            <span class="boxTitle"><b>Kontakta oss</b></span> 
+            <div class="flexdirectionrow" style="justify-content: center;">
+                <div class="contactTextBox">
+                    Smålandsgatan 53<br>
+                    710 00 Småland<br>
 
-            Tel: 010-123 45<br>
-            Email: info@madeinchina.se<br>
-            
-           
-         <img id="map-icon" src="map.jpg" alt="map icon">
-       
+                    Tel: 010-123 45<br>
+                    Email: info@madeinchina.se<br>
+                </div>
+                <div class="mapbox">   
+                    <img id="map-icon" src="@/assets/map.jpg" alt="map icon">
+                </div>
+            </div>
         </div>
         <div id="jobbPanel">
-    <h6><b>Jobba hos oss</b></h6>
-    <ul>
-    <li>Ekonomiavdelningen</li>
-    <li>Marknadsavdelning</li>
-    <li>Programavdelning</li>
-    <li>Driftavdelning</li>
-    <li>Försäljningsavdelning</li>
-    <li>HR avdelning</li>
-    <li>Sommarjobb och praktik</li>
-</ul>
+            <div class="flexdirectioncolumn">
+                <span class="boxTitle"><b>Jobba hos oss</b></span>
+                <ul>
+                    <li>Ekonomiavdelningen</li>
+                    <li>Marknadsavdelning</li>
+                    <li>Programavdelning</li>
+                    <li>Driftavdelning</li>
+                    <li>Försäljningsavdelning</li>
+                    <li>HR avdelning</li>
+                    <li>Sommarjobb och praktik</li>
+                </ul>
+            </div>
         </div>
 
 
         <div id="historyPanel">
-        <h6><b>Vår histora</b></h6>
-Made in China grundades år 1985 av Kalle Smålänning. Hans passion för film startade iden om att använda sitt arv från hans far till att starta Made in China.
-Namnet kommer från att hans far levde och arbetade i China under många år av sitt liv. 
-Made in China AB är ett av Smålands ledande leverantörer av kultur och nöje med ca 1 miljon biobesök per år. Vi ingår i Asian Cinemas Group som ägs av världens största biografkedja AMC Theatres.
-Made in Chinas huvudkontor ligger i Smålandsgatan 53inns våra stabsfunktioner:
-Ledning
-Ekonomiavdelning
-Marknadsavdelning
-Programavdelning
-Driftavdelning
-Försäljningsavdelning
-HR-avdelning
-
-</div>
-    
-    
+            <div class="flexdirectioncolumn">
+            <span class="boxTitle"><b>Vår histora</b></span>
+                <div>
+                    <div style="text-align: left; width: 60%; margin: 15px auto;">Made in China grundades år 1985 av Kalle Smålänning. Hans passion för film startade iden om att använda sitt arv från hans far till att starta Made in China.
+                    Namnet kommer från att hans far levde och arbetade i China under många år av sitt liv. <br>
+                    Made in China AB är ett av Smålands ledande leverantörer av kultur och nöje med ca 1 miljon biobesök per år. 
+                    Vi ingår i Asian Cinemas Group som ägs av världens största biografkedja AMC Theatres.<br>
+                    Made in Chinas huvudkontor ligger i Smålandsgatan 53inns våra stabsfunktioner:
+                    </div>
+                    <br>
+                    <ul>
+                        <li>Ledning</li>
+                        <li>Ekonomiavdelning</li>
+                        <li>Marknadsavdelning</li>
+                        <li>Programavdelning</li>
+                        <li>Driftavdelning</li>
+                        <li>Försäljningsavdelning</li>
+                        <li>HR-avdelning</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
 #kontaktPanel, #jobbPanel, #historyPanel{
     width: 95%;
-    background-color: #C62828;
+    background-color: #efefef;
+	color: #000;
     margin: 15px;
-    padding: 15px;
+    padding: 30px;
     border-radius: 5px;
-    height: 400px;
     text-align: center;
     font-style: normal;
     font-size: 14,5;
-    -webkit-text-fill-color: white;
-    
+    display: flex; 
+    justify-content: center;
+}
+.mapbox{
+    width: 200px;
+    height: 200px;
+    margin: 15px;
 }
 #map-icon{
  width:100%;
- height: 100%;
-    
+ height: 100%;    
+}
+.contactTextBox{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+.boxTitle{
+    text-align: center;
+    font-size: 20px;
+    margin: 20px;
 }
 </style>
